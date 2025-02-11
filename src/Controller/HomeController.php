00ee207f11b6de64ef8eacf4a639f2f84df48db0
neboysam <monte.nemi@gmail.com>
2025-02-11 09:40:24 +0100

    added database and user table

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -7,11 +7,9 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class HomeController extends AbstractController{
-    #[Route('/home', name: 'app_home')]
+    #[Route('/', name: 'app_home')]
     public function index(): Response
     {
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
-        ]);
+        return $this->render('home/index.html.twig');
     }
 }
