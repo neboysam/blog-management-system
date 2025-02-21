7c54411c836c1dbb404560622f1d47450dbf9836
neboysam <monte.nemi@gmail.com>
2025-02-21 13:53:34 +0100

    added features: add category and show all categories

--- src/Controller/Admin/CategoryController.php
+++ src/Controller/Admin/CategoryController.php
@@ -2,20 +2,43 @@
 
 namespace App\Controller\Admin;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Category;
+use App\Form\CategoryType;
+use App\Repository\CategoryRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 final class CategoryController extends AbstractController{
     #[Route('/admin/categories', name: 'app_admin_categories')]
-    public function index(): Response
+    public function showCategories(CategoryRepository $categoryRepo): Response
     {
-        return $this->render('admin/category/category.html.twig');
+        $categories = $categoryRepo->findAll();
+        return $this->render('admin/category/category.html.twig', [
+            'categories' => $categories
+        ]);
     }
 
     #[Route('/admin/add-category', name: 'app_admin_add_category')]
-    public function addCategory(): Response
+    public function addCategory(Request $request, EntityManagerInterface $entityManager): Response
     {
-        return $this->render('admin/category/add-category.html.twig');
+        $category = new Category();
+        $form = $this->createForm(CategoryType::class, $category);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($category);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Category ' . "'" . $category->getTitle() . "'" . ' is added successfully.');
+            return $this->redirectToRoute('app_admin_categories');
+        } 
+        
+        return $this->render('admin/category/add-category.html.twig', [
+            'categoryForm' => $form->createView()
+        ]);
     }
 }
---
+++ src/Form/CategoryType.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Category;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+class CategoryType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title', TextType::class, [
+                'label' => 'Title',
+                'attr' => [
+                    'placeholder' => 'Enter title'
+                ]
+            ])
+            ->add('description', TextType::class, [
+                'label' => 'Description',
+                'attr' => [
+                    'placeholder' => 'Enter description'
+                ]
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => 'Add Category',
+                'attr' => [
+                    'class' => 'btn btn-success'
+                ]
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Category::class,
+        ]);
+    }
+}
